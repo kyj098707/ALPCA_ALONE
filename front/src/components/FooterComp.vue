2cbed6446f6e49d38df0fc80a5c24f287ab7448d
kimyoonjong <kyj098707@naver.com>
2024-08-19 16:21:09 +0900

    feat: 로고 변경

--- front/src/components/FooterComp.vue
+++ front/src/components/FooterComp.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
+    <div id="footer-container">
         this footer;
     </div>    
 </template>
+
+<style scoped>
+    #footer-container {
+        color: rgb(130 135 153 / 1);
+    }
+</style>
